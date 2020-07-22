eb8fc0bf3bc2ed20411ed95798db1e44ce786468
Pearcee <steven@gmail.com>
2020-07-22 12:08:19 +0100

    comment

--- git.sh
+++ git.sh
@@ -1,8 +1,7 @@
-#   git config --global user.email "steven@gmail.com"
-#  git config --global user.name "Pearcee"
+
 
 comment = date +"%y.%m.%d.%T"
 git status
 git add --all .
 git commit -m comment
-git push -u origin master
+git push -u origin master git config --global user.name "Pearcee"
